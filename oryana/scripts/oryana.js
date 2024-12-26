c50ae010d84441ff5d9ab6034523e3705c91263d
Edward <97175962+Jegijs@users.noreply.github.com>
2024-12-26 18:00:40 +0200

    Add files via upload

---
+++ oryana/scripts/oryana.js
@@ -0,0 +1,7 @@
+function numberAlert () {
+    alert('(561) 575-2679')
+}
+
+
+//<div data-customgform="clt4mxo2100giwn5w0ejjo9kc"></div>
+//<script async src="https://cdn.customgform.com/cgf.js"></script>
